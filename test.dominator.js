64a3b6d44e25adfa94c78312eed56e8d3e501f86
Enrico <enrico.luz@corp.globo.com>
2010-07-06 00:33:34 -0300

    Changed test helper for easier validation of generated elements.

--- test.dominator.js
+++ test.dominator.js
@@ -1,30 +1,30 @@
 $(document).ready(function(){
     module('dominator');
 
-    function html (element) {
+    function html (selector) {
         /* Helper for innerHtml checking. */
-        return $('<div />').append(element).html();
+        return $('<div />').append($.dominator(selector)).html();
     }
 
     test('generate simple tag', function() {
         expect(2);
-        equals(html($.dominator('p')), '<p></p>', '"p" generates a p');
-        equals(html($.dominator('div')), '<div></div>', '"div" generates a div');
+        equals(html('p'), '<p></p>', '"p" generates a p');
+        equals(html('div'), '<div></div>', '"div" generates a div');
     });
 
     test('generate nested tags', function() {
         expect(2);
-        equals(html($.dominator('div p')), '<div><p></p></div>', '"div p" selector generates div/p');
-        equals(html($.dominator('div p span a')), '<div><p><span><a></a></span></p></div>', '"div p span a" selector generates div/p/span/a');
+        equals(html('div p'), '<div><p></p></div>', '"div p" selector generates div/p');
+        equals(html('div p span a'), '<div><p><span><a></a></span></p></div>', '"div p span a" selector generates div/p/span/a');
     });
 
     test('generate with id', function() {
-        equals(html($.dominator('div#myid')), '<div id="myid"></div>', '"div#myid" generates a div[id=myid]');
+        equals(html('div#myid'), '<div id="myid"></div>', '"div#myid" generates a div[id=myid]');
     });
 
     test('generate nested with id', function() {
         expect(2);
-        equals(html($.dominator('div p#myid')), '<div><p id="myid"></p></div>', '"div p#myid" generates a div/p[id=myid]');
-        equals(html($.dominator('div#myid p')), '<div id="myid"><p></p></div>', '"div#myid p" generates a div[id=myid]/p');
+        equals(html('div p#myid'), '<div><p id="myid"></p></div>', '"div p#myid" generates a div/p[id=myid]');
+        equals(html('div#myid p'), '<div id="myid"><p></p></div>', '"div#myid p" generates a div[id=myid]/p');
     });
 });
