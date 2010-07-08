8a0aadab6ad27153e4e6f952841012acd40396ff
Enrico <enrico.luz@corp.globo.com>
2010-07-07 22:54:35 -0300

    Refactored tests for better browser compatibility.

--- tests/test.dominator.js
+++ tests/test.dominator.js
@@ -1,47 +1,52 @@
 $(document).ready(function(){
     module('dominator');
 
-    function html (selector, context) {
-        /* Helper for innerHtml checking. */
-        return $('<div />').append($.dominator(selector, context)).html().toLowerCase();
+    function html (object) {
+        /* Helper for innerHtml. */
+        return $('<div />').append(object).html();
     }
 
-    function html_equals (selector, expected) {
+    function html_equals (object, expected) {
+        /* Helper for html checking. */
+        equals(html(object), html($(expected)));
+    }
+
+    function dom_equals (selector, expected) {
         /* Compares the resulting HTML with the expected. */
-        equals(html(selector), expected, '"' + selector + '" generates');
+        html_equals($.dominator(selector), expected);
     }
 
     test('generate simple tag', 2, function() {
-        html_equals('p', '<p></p>');
-        html_equals('div', '<div></div>');
+        dom_equals('p', '<p></p>');
+        dom_equals('div', '<div></div>');
     });
 
     test('generate nested tags', 2, function() {
-        html_equals('div p', '<div><p></p></div>');
-        html_equals('div p span a', '<div><p><span><a></a></span></p></div>');
+        dom_equals('div p', '<div><p></p></div>');
+        dom_equals('div p span a', '<div><p><span><a></a></span></p></div>');
     });
 
     test('generate with id', function() {
-        html_equals('div#myid', '<div id="myid"></div>');
+        dom_equals('div#myid', '<div id="myid"></div>');
     });
 
     test('generate nested with id', 2, function() {
-        html_equals('div p#myid', '<div><p id="myid"></p></div>');
-        html_equals('div#myid p', '<div id="myid"><p></p></div>');
+        dom_equals('div p#myid', '<div><p id="myid"></p></div>');
+        dom_equals('div#myid p', '<div id="myid"><p></p></div>');
     });
 
     test('generate tagless id', function() {
-        html_equals('#myid', '<div id="myid"></div>');
+        dom_equals('#myid', '<div id="myid"></div>');
     });
 
     test('generate with class', function() {
-        html_equals('div.myclass', '<div class="myclass"></div>');
-        html_equals('div.my-class', '<div class="my-class"></div>');
-        html_equals('div.my_class', '<div class="my_class"></div>');
+        dom_equals('div.myclass', '<div class="myclass"></div>');
+        dom_equals('div.my-class', '<div class="my-class"></div>');
+        dom_equals('div.my_class', '<div class="my_class"></div>');
     });
 
     test('generate with tagless class', function() {
-        html_equals('.myclass', '<div class="myclass"></div>');
+        dom_equals('.myclass', '<div class="myclass"></div>');
     });
 
     test('generate with class and id', 2, function() {
@@ -57,16 +62,16 @@ $(document).ready(function(){
     });
 
     test('generate multiple classes', function() {
-        html_equals('div.first.second', '<div class="first second"></div>');
+        dom_equals('div.first.second', '<div class="first second"></div>');
     });
 
     test('multiple selectors', 2, function() {
-        html_equals('div, p', '<div></div><p></p>');
-        html_equals('div#id, p.class a', '<div id="id"></div><p class="class"><a></a></p>');
+        dom_equals('div, p', '<div></div><p></p>');
+        dom_equals('div#id, p.class a', '<div id="id"></div><p class="class"><a></a></p>');
     });
 
     test('parse simple attribute', function() {
-        html_equals('a[title=simpletitle]', '<a title="simpletitle"></a>');
+        dom_equals('a[title=simpletitle]', '<a title="simpletitle"></a>');
     });
 
     test('parse consecutive attributes', 2, function() {
@@ -76,6 +81,6 @@ $(document).ready(function(){
     });
 
     test('allow variables', function() {
-        equals(html('a[name=${myvar}]', {myvar: 'myvariable'}), '<a name="myvariable"></a>');
+        html_equals($.dominator('a[name=${myvar}]', {myvar: 'myvariable'}), '<a name="myvariable"></a>');
     });
 });
